ebf47c51f9d04f0ab9b05140df22c51ffb72ae7c
DnznJuan <113405275+DnznJuan@users.noreply.github.com>
2022-11-25 22:02:15 -0600

    Update case

--- pages/api/[tokenId].js
+++ pages/api/[tokenId].js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     //get token ID from params
-    const tokenID = req.query.tokenID
+    const tokenID = req.query.tokenId
     const image_url = "https://raw.githubusercontent.com/DnznJuan/lw3-soph-nftcollection/87a0fd4cce4518e48ddc950da35e74e8eacc3a41/public/cryptodevs/"
     // SEND METADATA W/OPENSEA/STANDARDS
     res.status(200).json ({
